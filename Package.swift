0734b68589d636c8fd7ffa8c60028542378d46b1
Darkzwer123 <137776351+Darkzwer123@users.noreply.github.com>
2024-06-03 17:49:41 +0300

    [samepkg]

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,18 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Full2",
-            targets: ["Full2"]),
+            targets: ["Full2", "FaceSDK"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Darkzwer/FullCore.git", .exact("7.2.9102")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Full2",
-            dependencies: []),
+        .target(name: "Full2", dependencies: ["FullCore"]),
+        .binaryTarget(name: "FaceSDK", url: "https://pods.regulaforensics.com/FaceSDK/6.1.1825/FaceSDK-6.1.1825.zip", checksum: "9b313d81bf540035f0bcd059ec5612fe517fd841746225486f397c6c65a88719"),
         .testTarget(
             name: "Full2Tests",
             dependencies: ["Full2"]),
